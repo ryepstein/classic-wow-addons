16d62e52cbc594edbfeda63047471245e2481d1e
Ryan Epstein <ryepstein@hotmail.com>
2019-10-03 19:54:08 -0700

    added ideas in block comment

---
+++ Sandbox.lua
@@ -0,0 +1,18 @@
+Sandbox = { }
+
+function Sandbox:HelloWorld()
+    message("Hello World!")
+end
+
+function Sandbox:HideGryphons()
+    MainMenuBarLeftEndCap:Hide()
+    MainMenuBarLeftEndCap:Hide()
+end
+
+--[[
+    IDEAS:
+      * Bag Slot indicator/warning when nearly low on bags
+      * Get Player Guild info (?) and make better unit frames
+      * Shaman totem info, clear totems?
+--]]
+Sandbox:HelloWorld()
